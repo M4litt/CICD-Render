2b9b9cd43ae3b1b45e5a11506e2572f21bb8a6ed
tomasbarak <barakcinthia@gmail.com>
2023-08-28 15:47:27 -0300

    Added nothin

--- test.js
+++ test.js
@@ -15,9 +15,11 @@ describe("App Tests", function () {
   after(async () => {
     await driver.quit();
   });
+
   it("Login successful", async () => {
     //Go to url
     await driver.get("http://cicd-render-yn7q.onrender.com/user");
+
     //Find element by id
     await driver.findElement(By.id("Username")).sendKeys("leto");
     //Find password by id
@@ -35,6 +37,7 @@ describe("App Tests", function () {
 
   it("data submition", async () => {
     await driver.get("http://cicd-render-yn7q.onrender.com/hub");
+
     await driver.findElement(By.name("name")).sendKeys("thiago");
 
     await driver.findElement(By.name("surname")).sendKeys("leto");
@@ -42,5 +45,14 @@ describe("App Tests", function () {
     await driver.findElement(By.name("age")).sendKeys("18");
 
     await driver.findElement(By.id("submit")).click();
+
+    await driver.sleep(1000);
+
+    assert.isTrue(
+      await driver.findElement(By.id("result")).getText().length > 0,
+      "result is empty"
+    )
+
+
   });
 });
