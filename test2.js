23bf16c32a7c13b326a708be37b2e3983908e74b
tomasbarak <barakcinthia@gmail.com>
2023-08-28 16:20:40 -0300

    localhost

--- test2.js
+++ test2.js
@@ -1,52 +1,63 @@
-const assert = require('assert');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, Key } = require("selenium-webdriver");
+
+const chai = require("chai");
+const { describe, it, before, after } = require("node:test");
+
+var assert = chai.assert;
+
 const chrome = require('selenium-webdriver/chrome');
 
 const options = new chrome.Options();
-options.headless(); // sin interfaz gráfica
+options.headless();
 
-const driver = new Builder()
-  .forBrowser('chrome')
-  .setChromeOptions(options)
-  .build();
-
-  describe('Web App Tests', function () {
+describe("App Tests", function () {
   this.timeout(30000); 
-
+  
   let driver;
 
-  before(async function () {
-    driver = await new Builder().forBrowser('chrome').build();
+  before(async () => {
+    driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
   });
 
-  after(async function () {
+  after(async () => {
     await driver.quit();
   });
 
-  it('Login successful', async function () {
+  it("Login successful", async () => {
     //Go to url
-    await driver.get("http://localhost:1461/user");
+    await driver.get("https://cicd-render-yn7q.onrender.com/user");
+
     //Find element by id
     await driver.findElement(By.id("Username")).sendKeys("leto");
     //Find password by id
     await driver.findElement(By.id("Password")).sendKeys("lamamadejoakod");
     //Find button by id
     await driver.findElement(By.id("login")).click();
+
     await driver.sleep(1000);
+
     //Find element by id
     let text_got = await driver.findElement(By.id("welcome")).getText();
+
     assert.equal(text_got, "HOLA LETO!!111!1");
   });
 
-  it('Form submition', async function () {
-    await driver.get("http://localhost:1461/hub");
+  it("data submition", async () => {
+    await driver.get("https://cicd-render-yn7q.onrender.com/hub");
+
     await driver.findElement(By.name("name")).sendKeys("thiago");
+
     await driver.findElement(By.name("surname")).sendKeys("leto");
+
     await driver.findElement(By.name("age")).sendKeys("18");
+
     await driver.findElement(By.id("submit")).click();
+
     await driver.sleep(1000);
+
     //Check if result is empty
     let text_got = await driver.findElement(By.id("result")).getText();
-    assert.equal(text_got.includes("thiago"), true);
+
+    assert.isTrue(text_got.includes("thiago"));
   });
 });
